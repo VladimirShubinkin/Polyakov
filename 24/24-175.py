aa9d3f3cd3d288a3b9a4c209fefe2ca01cd187d2
vladimir <vladimir.shubinkin@gmail.com>
2023-02-03 10:52:32 +0300

    В названии изменил "-" на "."

--- 24/24-175.py
+++
@@ -1,9 +0,0 @@
-with open('24-175.txt') as f:
-    a = [len(s) for s in f.read().strip().split('KEGE')]
-
-max_len = 0
-for i in range(1, len(a) - 3):
-    length = sum(a[i:i + 3]) + 6 + 8
-    max_len = max(max_len, length)
-max_len = max(max_len, sum(a[:3]) + 3 + 8, sum(a[-3:]) + 3 + 8)
-print(max_len)
