600d634eccc6cf0717e9812d948b53b59d208dd6
VladimirShubinkin <vladimir.shubinkin@gmail.com>
2023-03-22 11:16:41 +0300

    24.234 вариант 2

--- 24/24.234.py
+++ 24/24.234.py
@@ -7,6 +7,7 @@ good = {}
 bad = set()
 with open('24-234.txt') as f:
     s = f.read().strip()
+# Вариант 1 (перебор чисел строки, медленнее)
 for i in range(len(s) - 6):
     for j in range(i, i + 6):
         ss = s[i:j + 1]
@@ -21,5 +22,13 @@ for i in range(len(s) - 6):
 max_num = max(good, key=int)
 print(max_num)
 print(good[max_num])
-for key, val in sorted(good.items(), key=lambda pair: -int(pair[0])):
-    print(key, val)
+# for key, val in sorted(good.items(), key=lambda pair: -int(pair[0])):
+#     print(key, val)
+
+# Вариант 2 (перебор подходящих чисел, быстрее)
+for n in range(10**6, 0, -1):
+    if sum_pow(str(n)) == n:
+        c = s.count(str(n))
+        if c:
+            print(c, n)
+            break
