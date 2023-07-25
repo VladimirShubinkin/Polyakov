10a03a1570ea519ff3bec705df2411ff0039fa28
VladimirShubinkin <vladimir.shubinkin@gmail.com>
2023-07-25 09:45:26 +0300

    Доп. файл с переводом в дополнительный двоичный код с лаконичной функцией

--- Дополнительный двоичный код.py
+++ Дополнительный двоичный код.py
@@ -15,13 +15,20 @@ def binar(x, n):
     return f'{ans:0{n}}'
 
 
+def good_binar(x, n):
+    ans = ""
+    for i in range(n):
+        ans = "01"[x % 2] + ans
+        x //= 2
+    return ans
+
 a = int(input())
 n = int(input())
 if a >= 0:
     print(binar(a, n))
 else:
     print(neg_binar(a, n))
-
-print(f'{a:0{n}b}')
-print(bin(a)[2:].zfill(n))
+print(good_binar(a, n))
+# print(f'{a:0{n}b}')
+# print(bin(a)[2:].zfill(n))
 
