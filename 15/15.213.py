3d89a533961d73ef491c139986e82d1ecd74233a
vladimir <vladimir.shubinkin@gmail.com>
2023-12-09 10:25:12 +0300

    Добавил 15.213

---
+++ 15/15.213.py
@@ -0,0 +1,14 @@
+def f(x, a):
+    return ((((x & 56 != 0) <= (x & 18 != 0)) or (x & a != 0))
+            <= ((x & 18 == 0) and (x & a == 0) and (x & 43 != 0)))
+
+def is_good(a):
+    for x in range(1, 64):
+        if f(x, a):
+            return False
+    return True
+
+a = 50
+while not is_good(a):
+    a += 1
+print(a)
--- 15/15.303.py
+++ 15/15.303.py
@@ -1,13 +1,14 @@
-def f(a):
+def f(x, y, a):
+    return (3 * y - x > a) or (2 * x + 3 * y < 30) or (2 * y - x < -31)
+
+def is_good(a):
     for x in range(1, 1000):
         for y in range(1, 1000):
-            if not((3 * y - x > a) or (2 * x + 3 * y < 30)
-                   or (2 * y - x < -31)):
+            if not f(x, y, a):
                 return False
     return True
 
-
 a = 1000
-while not f(a):
+while not is_good(a):
     a -= 1
 print(a)
