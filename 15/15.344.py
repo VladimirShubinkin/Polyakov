3a56d20bef3a3836596c299eb87334cac143ac85
vladimir <vladimir.shubinkin@gmail.com>
2023-12-14 11:39:54 +0300

    Добавил 15.344

---
+++ 15/15.344.py
@@ -0,0 +1,14 @@
+def f(x, y, a):
+    return (5 * x + 3 * y != 60) or (a > x) and (a > y)
+
+def is_good(a):
+    for x in range(1000):
+        for y in range(1000):
+            if not f(x, y, a):
+                return False
+    return True
+
+a = 1
+while not is_good(a):
+    a += 1
+print(a)
