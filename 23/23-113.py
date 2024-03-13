3f9b12fd4721d5de0bbc80a34627eb8fbf7a2904
VladimirShubinkin <vladimir.shubinkin@gmail.com>
2024-03-13 16:34:08 +0300

    23

--- 23/23-113.py
+++ 23/23-113.py
@@ -24,3 +24,17 @@ def f(a, k):
 
 f(1, 0)
 print(min(lens))
+
+
+def g(a, b, k):
+    if a > b or k < 0:
+        return 0
+    if a == b and k == 0:
+        return 1
+    return g(a + 1, b, k - 1) + g(a + 5, b, k - 1) + g(a * 3, b, k - 1)
+
+
+k = 0
+while not g(1, 227, k):
+    k += 1
+print(k)
