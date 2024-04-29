d6a450a26d785c2113b1c8c9f60374b23bea46dc
VladimirShubinkin <vladimir.shubinkin@gmail.com>
2024-04-29 10:33:36 +0300

    Переводы СС

--- funcs.py
+++ funcs.py
@@ -1,10 +1,31 @@
-def to_base(a, n):
-    res = ''
-    while a:
-        res = str(a % n) + res
-        a //= n
-    return res
+from string import digits, ascii_uppercase
+alphabet = digits + ascii_uppercase
+
+
+def from_dec(n: int, base: int) -> str:
+    result = ''
+    while n:
+        result = alphabet[n % base] + result
+        n //= base
+    return result
+
+
+def from_dec_list(n: int, base: int) -> list[int]:
+    result = []
+    while n:
+        result = [n % base] + result
+        n //= base
+    return result
+
+
+def to_dec(digits: list[int], base: int) -> int:
+    result = 0
+    power = 1
+    for d in digits[::-1]:
+        result += d * power
+        power *= base
+    return result
 
 
 if __name__ == '__main__':
-    print(to_base(100_000_000, 3))
+    print()
