691dbd672f04941ffe73e35728f0446e0c237a07
VladimirShubinkin <vladimir.shubinkin@gmail.com>
2024-06-06 19:18:08 +0300

    внёс правки

--- 27/27-124.py
+++ 27/27-124.py
@@ -5,13 +5,14 @@ with open('files/27-124b.txt') as f:
         km, kg = map(int, f.readline().split())
         a[km % k] = kg // v + bool(kg % v)
 # считаем ответ для нулевого километра
-s = 0
-for i in range(-m, m + 1):
-    s += a[i]
+# s = 0
+# for i in range(-m, m + 1):
+#     s += a[i]
+s = sum(a[-m:]) + sum(a[:m + 1])
 max_s = s if a[0] else 0
 # "переставляем" почтовое отделение
 for i in range(1, k):
-    s = s - a[i - m] + a[(i + m) % k]
+    s = s - a[i - m - 1] + a[(i + m) % k]
     if a[i]:
         max_s = max(max_s, s)
 print(max_s)
