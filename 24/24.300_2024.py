9ca98cf9a3632cd7ed5b167de213cc6cb803ce2b
vladimir <vladimir.shubinkin@gmail.com>
2024-10-18 13:18:22 +0300

    внёс правки

--- 24/24.300_2024.py
+++ 24/24.300_2024.py
@@ -14,7 +14,12 @@ s = s.replace('*+', '*$+')
 lines = s.split('$')
 ans_str = ''
 for line in lines:
-    cur_line = line.lstrip('0')  # Избавились от возможных нолей в начале
+    cur_line = line
+    while cur_line[0] == '0':  # Избавляемся от возможных нолей в начале
+        if cur_line[1] in '+*':
+            break
+        cur_line = cur_line[1:]
+    # cur_line = line.lstrip('0') - нельзя из-за случая 0+ или 0*, если изначально было +00+
     cur_line = cur_line.strip('+*')
     cur_str = ''
     for term in cur_line.split('+'):
