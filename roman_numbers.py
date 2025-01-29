55513b7d1afdaff04ffe4dcfba9dfc6a9334d96e
VladimirShubinkin <vladimir.shubinkin@gmail.com>
2025-01-29 13:08:09 +0300

    Правки в roman_numbers

--- roman_numbers.py
+++ roman_numbers.py
@@ -1,4 +1,4 @@
-class Roman_number():
+class RomanNumber:
     def __init__(self, inp: int|str):
         self.to_replace = (('CM', 'DCCCC'), ('CD', 'CCCC'), ('XC', 'LXXXX'),
                            ('XL', 'XXXX'), ('IX', 'VIIII'), ('IV', 'IIII'))
@@ -12,19 +12,19 @@ class Roman_number():
             self.dec_num = self.to_arabic()
 
     def __add__(self, other):
-        return Roman_number(self.dec_num + other.dec_num)
+        return RomanNumber(self.dec_num + other.dec_num)
 
     def __iadd__(self, other):
-        return Roman_number(self.dec_num + other.dec_num)
+        return RomanNumber(self.dec_num + other.dec_num)
 
     def __imul__(self, other):
-        return Roman_number(self.dec_num * other.dec_num)
+        return RomanNumber(self.dec_num * other.dec_num)
 
     def __mul__(self, other):
-        return Roman_number(self.dec_num * other.dec_num)
+        return RomanNumber(self.dec_num * other.dec_num)
 
     def __repr__(self):
-        return f"Roman_number('{self.rom_num}')"
+        return f"RomanNumber('{self.rom_num}')"
 
     def __str__(self):
         return self.rom_num
@@ -66,11 +66,11 @@ class Roman_number():
 
 
 def roman_to_arabic(roman_num: str) -> int:
-    return Roman_number(roman_num).dec_num
+    return RomanNumber(roman_num).dec_num
 
 
-def arabic_to_roman(num: int) -> Roman_number:
-    return Roman_number(num)
+def arabic_to_roman(num: int) -> RomanNumber:
+    return RomanNumber(num)
 
 # print(roman_to_arabic('XXX'))
 # print(roman_to_arabic('XIX'))
@@ -86,12 +86,12 @@ def arabic_to_roman(num: int) -> Roman_number:
 # print(arabic_to_roman(9))
 # print(arabic_to_roman(30))
 
-print(Roman_number(15) + Roman_number('IX'))
-print(int(Roman_number('XXX')))
-print(Roman_number('IV') >= Roman_number(5))
-a = Roman_number(33)
-b = Roman_number('III')
-a += b
-print(a)
-b *= a
-print(a, b)
+# print(RomanNumber(15) + RomanNumber('IX'))
+# print(int(RomanNumber('XXX')))
+# print(RomanNumber('IV') >= RomanNumber(5))
+# a = RomanNumber(33)
+# b = RomanNumber('III')
+# a += b
+# print(a)
+# b *= a
+# print(a, b)
