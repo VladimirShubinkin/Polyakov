8cef829fbea91c8d51dd2bfe3dccdc8b932ddd22
VladimirShubinkin <vladimir.shubinkin@gmail.com>
2025-03-03 20:10:35 +0300

    24.154_re испр

--- 24/24.154_re.py
+++ 24/24.154_re.py
@@ -3,6 +3,7 @@ import re
 with open('24data/24-153.txt') as f:
     s = f.read().strip()
 
-min_seq = min(re.findall(r'D[^D]+D', s), key=len)
-print(len(min_seq))
+pattern = r'(?<=D)[^D]+(?=D)'
+min_seq = min(re.findall(pattern, s), key=len)
+print(len(min_seq) + 2)
 print(min_seq)
