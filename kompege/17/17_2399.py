25a373cdd4216d6831f6fd1052e9d6a592f5449f
VladimirShubinkin <vladimir.shubinkin@gmail.com>
2025-03-21 09:06:34 +0300

    17.270 (kompege 17_2399)

---
+++ kompege/17/17_2399.py
@@ -0,0 +1,16 @@
+with open('../../17/17-243.txt') as f:
+    a = list(map(int, f.readlines()))
+sum_digits = 0
+for x in a:
+    if x % 35 == 0:
+        sum_digits += sum(map(int, str(x)))
+count = 0
+min_sum = 10**10
+for i in range(len(a) - 1):
+    # if ((a[i] > sum_digits and a[i + 1] <= sum_digits and a[i + 1] % 16**2 == int('EF', 16))
+    #         or (a[i + 1] > sum_digits  and a[i] <= sum_digits and a[i] % 16**2 == int('EF', 16))):
+    if ((a[i] > sum_digits and a[i + 1] <= sum_digits  and f'{a[i + 1]:X}'[-2:] == 'EF')
+            or (a[i + 1] > sum_digits and a[i] <= sum_digits and f'{a[i]:X}'[-2:] == 'EF')):
+        count += 1
+        min_sum = min(min_sum, a[i] + a[i + 1])
+print(count, min_sum)
