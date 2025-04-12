f58d97b7dfc09dcd412d6badeaab67ffd97a4d06
vladimir <vladimir.shubinkin@gmail.com>
2025-04-12 13:59:14 +0300

    24 re досрок 2025

--- 24/24.268_re.py
+++ 24/24.268_re.py
@@ -1,7 +1,7 @@
 import re
 
 with open('24data/24-268.txt') as f:
-    s = f.read().strip()
+    s = 'Z' + f.read().strip() + 'Z'
 pattern = r'(?<=[K-Z])[1-9A-J][0-9A-J]*[02468ACEGI](?=[K-Z])'
 max_seq = max(re.findall(pattern, s), key=lambda n: int(n, 20))
 print(max_seq)
